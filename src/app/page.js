ee0fb0f9b3eef6f9168d824034e2942944f01977
priyansh2120 <priyanshkumar50@gmail.com>
2024-02-18 02:23:33 +0530

    added default export

--- src/app/page.js
+++ src/app/page.js
@@ -1,89 +1,72 @@
-"use client"
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
-const page = () => {
-  const [name, setName] = useState('');
-  const [cid, setCID] = useState('');
+const Mypage = () => {
+  const [name, setName] = useState("");
+  const [cid, setCID] = useState("");
 
   async function verify() {
-    console.log("Fetching proofs")
-    const data = await fetch(`https://${cid}.ipfs.dweb.link/`)
-    const json = await data.json()
+    console.log("Fetching proofs");
+    const data = await fetch(`https://${cid}.ipfs.dweb.link/`);
+    const json = await data.json();
     if (json.studentName == name) {
-      alert ("Verified")
-    }
-    else {
-      alert("Could not Verify. Failging singatures")
+      alert("Verified");
+    } else {
+      alert("Could not Verify. Failging singatures");
     }
   }
 
   return (
-    <div className='body'>
-
-      <div className='navbar'>
-        <div className='logo'>
-          <h1>
-            Job Hunt
-          </h1>
+    <div className="body">
+      <div className="navbar">
+        <div className="logo">
+          <h1>Job Hunt</h1>
         </div>
 
-        <div className='search'>
-          <h2>
-            Search
-          </h2>
+        <div className="search">
+          <h2>Search</h2>
         </div>
 
-        <div className='post'>
-          <h2>
-            Post
-          </h2>
+        <div className="post">
+          <h2>Post</h2>
         </div>
       </div>
-      <div className='wrap'>
+      <div className="wrap">
         <div className="nameEnter">
-          <p>
-            Your Name:
-          </p>
+          <p>Your Name:</p>
           <input
             type="text"
-            onChange={((e) => { setName(e.target.value) })}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
         </div>
 
         <div className="cidEnter">
-          <p>
-            Enter your CID:
-          </p>
+          <p>Enter your CID:</p>
           <input
             type="text"
-            onChange={((e) => { setCID(e.target.value) })}
+            onChange={(e) => {
+              setCID(e.target.value);
+            }}
           />
         </div>
 
         <div className="dobEnter">
-          <p>
-            Enter your DOB:
-          </p>
-          <input
-            type="text"
-            placeholder='DD/MM/YYYY'
-          />
+          <p>Enter your DOB:</p>
+          <input type="text" placeholder="DD/MM/YYYY" />
         </div>
 
         <div className="dobEnter">
-          <p>
-            Enter Father Name:
-          </p>
-          <input
-            type="text"
-          />
+          <p>Enter Father Name:</p>
+          <input type="text" />
         </div>
-        <button onClick={verify} className='verify'>Verify</button>
+        <button onClick={verify} className="verify">
+          Verify
+        </button>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default page;
+export default Mypage;
